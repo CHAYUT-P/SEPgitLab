4b4c9361d3005cd29f97097b1daca1849adead9a
PALM. <68790385+CHAYUT-P@users.noreply.github.com>
2025-12-23 17:35:48 +0700

    Images and python file

---
+++ Lab5_1/L.py
@@ -0,0 +1,27 @@
+import sys
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+
+class Simple_drawing_window1(QWidget):
+    def __init__(self):
+        QWidget.__init__(self,None)
+        self.setWindowTitle("Simple")
+        self.ball = QPixmap("images/ball.png")
+    
+    def paintEvent(self,e):
+        p = QPainter()
+        p.begin(self)
+        p.drawPixmap(QRect(200,100,320,320),self.ball)
+        p.end()
+
+def main():
+    app = QApplication(sys.argv)
+
+    w = Simple_drawing_window1()
+    w.show()
+
+    return app.exec()
+
+if __name__ == "__main__":
+    sys.exit(main())
